be1b8c0cdd62b68ecddfff8a9ba7ae627497735b
AltinTorba <alabto@gmail.com>
2026-06-17 23:54:53 +0200

    Final project cleanup: remove db, fix settings, add throttling and PAGE_SIZE

--- core/settings.py
+++ core/settings.py
@@ -102,7 +102,15 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 6,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day',
+    }
 }
 
 MEDIA_URL = '/media/'
